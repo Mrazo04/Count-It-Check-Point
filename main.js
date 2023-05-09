1c936fdc28a063103be66dd9b29db73b6d175595
Mrazo04 <munguiamickey@gmail.com>
2023-05-09 14:52:32 -0500

    added function to count letters in word

---
+++ main.js
@@ -0,0 +1,18 @@
+const countIt = (word) => {
+  word= word.trim().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/ +/g, "");
+  let count = {};
+
+  // looping through the items
+  for (let i = 0; i < word.length; i++) {
+    // check if the character is at that position
+    const letter = word[i];
+
+    if (!count[letter]) {
+      count[letter] = 1;
+    } else {
+      count[letter]++;
+    }
+  }
+  return count;
+};
+console.log(countIt("    ham burger!"));
