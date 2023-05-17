4b23d362304ad527ef3aeb571d6daf5830d7638e
Mrazo04 <munguiamickey@gmail.com>
2023-05-17 07:58:08 -0500

    added DOM

--- index.js
+++ index.js
@@ -1,45 +1,34 @@
-let word = "";
-let input = document.getElementById("wordInput");
+
 const button = document.getElementById("countButton")
 const reset = document.getElementById("resetButton")
 let results = document.getElementById("result")
 
-//console.log()
+const countIt = (word) => {
+  word = document.getElementById("word").value
+  let count = {};
 
-//eventlisteners
+  // looping through the items
+  for (let i = 0; i < word.length; i++) {
+    // check if the character is at that position
+    const letter = word[i];
 
-input.addEventListener("keyup", (e) => {
-    word = e.target.value;
-    console.log(input.value);
-});
+    if (!count[letter]) {
+      count[letter] = 1;
+    } else {
+      count[letter]++;
+    }
+  }
 
-button.addEventListener("click", (e) => {
-    console.log("click");
-
-    const countIt = (word) => {
-        word= word.trim().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/ +/g, "");
-        let count = {};
-      
-        // looping through the items
-        for (let i = 0; i < word.length; i++) {
-          // check if the character is at that position
-          const letter = word[i];
-      
-          if (!count[letter]) {
-            count[letter] = 1;
-          } else {
-            count[letter]++;
-          }
-        }
-        return count;
-    };
-    console.log(countIt(word))
-    results.innerText
-});
-
+  console.log(JSON.stringify(count).length);
+  
+  if ((JSON.stringify(count).length)>0){
+    results.innerHTML= JSON.stringify(count);
+  }else{
+    results.innerHTML= "";
+  }
+};
 
 reset.addEventListener("click", (e) => {
-    input.value = '';
     results.innerText = '';
     word = '';
     console.log("click");
